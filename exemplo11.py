87ab7f2ef30196518ce7a9ec8de22159c3901757
SERGIO GUSTAVO MEDINA PEREIRA <162819769+medinasergiogustavo@users.noreply.github.com>
2024-03-18 19:51:44 -0300

    Continando os trabalhos

---
+++ exemplo11.py
@@ -0,0 +1,4 @@
+#Comentario de uma linha, tudo que estiver a frente da cerquilha será ignorado
+print("Aprendendo como utilizar comentários.");
+print("segunda linha");
+print("terceira linha");#aqui é um comentário que não será interpretado
